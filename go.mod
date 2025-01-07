185ce48e62de060c7403f83acbc02ce444a60d17
Teerut26 <teerutsr@gmail.com>
2025-01-08 01:30:09 +0700

    Add dotenv support and update LINE Messaging API integration

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/teerut26/lucky-shirt-go
 
 go 1.23.4
 
-require github.com/line/line-bot-sdk-go/v8 v8.10.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/line/line-bot-sdk-go/v8 v8.10.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/line/line-bot-sdk-go/v8/linebot/messaging_api"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("err loading: %v", err)
+	}
+	client := &http.Client{}
 	bot, err := messaging_api.NewMessagingApiAPI(
-		os.Getenv("LINE_CHANNEL_TOKEN"),
+		os.Getenv("CHANNEL_ACCESS_TOKEN"),
+		messaging_api.WithHTTPClient(client),
 	)
 
 	if err != nil {
 		panic(err)
 	}
 
+	// broadcast message
+	broadcastMessage := &messaging_api.BroadcastRequest{
+		Messages: []messaging_api.MessageInterface{
+			messaging_api.TextMessage{
+				Text: "asdfasdf",
+			},
+		},
+	}
+	_, err = bot.Broadcast(broadcastMessage, "")
+	if err != nil {
+		panic(err)
+	}
 }
